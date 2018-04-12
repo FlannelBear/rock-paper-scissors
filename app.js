94accdc9c4963aa9b489c570d5f15c3aaa271527
Flannel Bear <josh.flannelbear@gmail.com>
2018-04-12 17:11:37 -0500

    html and css wip, app.js completed and functional

---
+++ app.js
@@ -0,0 +1,61 @@
+// Get user choice
+function getUserChoice(userInput) {
+  userInput = userInput.toLowerCase();
+  if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+    return userInput;
+  } else {
+    return 'Error!';
+  }
+}
+// Get computer choice
+function getComputerChoice() {
+  randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber){
+    case 0:
+    return 'rock';
+    case 1:
+    return 'paper';
+    case 2:
+    return 'scissors';
+    default:
+    return 'error';
+  }
+}
+// Determine Winner
+function determineWinner(userChoice, computerChoice) {
+  if(userChoice === computerChoice){
+    return 'Tie!';
+  }
+
+  if(userChoice !== computerChoice){
+    if(userChoice === 'rock'){
+      if(computerChoice === 'scissors'){
+        return 'You win!';
+      } else {
+        return 'Computer wins.!';
+      }
+    } else if(userChoice === 'paper'){
+      if(computerChoice === 'rock'){
+        return 'You win!';
+      } else {
+        return 'Computer wins.!';
+      }
+    } else if(userChoice === 'scissors'){
+      if(computerChoice === 'paper'){
+        return 'You win!';
+      } else {
+        return 'Computer wins.';
+      }
+    }
+  }
+}
+// Play game
+function playGame(){
+  var userChoice = getUserChoice('rock');
+  var computerChoice = getComputerChoice();
+  console.log('You chose ' + userChoice);
+  console.log('The computer chose ' + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+  playGame();
